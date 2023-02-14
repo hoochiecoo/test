65dc87b5a75de083b95911f817a61b35d6225219
hoochiecoo <waveinspectro@gmail.com>
2023-02-15 02:30:17 +0300

    Update script2.js

--- pc/moex3/script2.js
+++ pc/moex3/script2.js
@@ -1,15 +1,21 @@
+let securitiesData;
+
 function fetchData() {
   fetch('https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities.json')
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      const metadataList = document.createElement('ul');
-      data['securities']['metadata'].forEach(item => {
-        const metadataItem = document.createElement('li');
-        metadataItem.textContent = `${item.name}: ${item.value}`;
-        metadataList.appendChild(metadataItem);
-      });
-      document.getElementById('console').appendChild(metadataList);
+      securitiesData = data;
+      console.log(securitiesData);
     })
     .catch(error => console.error(error));
+}
+
+function showMetadata() {
+  const metadataList = document.createElement('ul');
+  securitiesData['securities']['metadata'].forEach(item => {
+    const metadataItem = document.createElement('li');
+    metadataItem.textContent = `${item.name}: ${item.value}`;
+    metadataList.appendChild(metadataItem);
+  });
+  document.getElementById('console').appendChild(metadataList);
 }
