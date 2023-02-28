ddff415c8a99db4062fdc1cd0246f6b5ce7380d0
Anatoly <waveinspectro@gmail.com>
2023-03-01 00:17:50 +0300

    Create script.js

---
+++ land1/script.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('#header').load('header.html');
+  $('#hero').load('hero.html');
+  $('#courses').load('courses.html');
+  $('#reviews').load('reviews.html');
+  $('#interactive').load('interactive.html');
+  $('#about').load('about.html');
+  $('#contact').load('contact.html');
+  $('#footer').load('footer.html');
+});
