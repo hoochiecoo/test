44863c1786baf29c28d65017e00dde64fcb223c0
Anatoly <waveinspectro@gmail.com>
2023-03-10 20:04:04 +0300

    Update v3.js

--- video/script-in-qp/v3.js
+++ video/script-in-qp/v3.js
@@ -1,50 +1,69 @@
-// check if browser supports getUserMedia method
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  
-  // define constraints for accessing camera
-  var constraints = {
-    video: true
-  };
+// Get video element from the DOM
+const videoElement = document.querySelector('video');
 
-  // access camera using getUserMedia method
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then(function(mediaStream) {
-      
-      // get video element on page
-      var video = document.getElementById('video');
+// Create canvas element to draw video frames
+const canvasElement = document.createElement('canvas');
+const canvasContext = canvasElement.getContext('2d');
 
-      // set video source to the media stream from the camera
-      video.srcObject = mediaStream;
+// Define initial zoom level
+let zoomLevel = 1;
 
-      // play the video
-      video.play();
+// Get user permission to access camera and start video stream
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    videoElement.srcObject = stream;
+    videoElement.play();
+  })
+  .catch(error => console.error(error));
 
-      // get camera track from media stream
-      var track = mediaStream.getVideoTracks()[0];
+// Wait for video to finish loading and update canvas dimensions
+videoElement.addEventListener('loadedmetadata', () => {
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
+});
 
-      // check if camera supports zooming
-      if('getCapabilities' in track) {
-        var capabilities = track.getCapabilities();
+// Parse the feature names from the URL query parameter
+const urlParams = new URLSearchParams(window.location.search);
+const featureNames = urlParams.get('toggle') ? urlParams.get('toggle').split(',') : [];
 
-        // check if camera supports zoom
-        if('zoom' in capabilities) {
-          var zoomControl = document.getElementById('zoom');
+// Define the feature functions
+const features = {
+  zoomIn: () => {
+    // Increase the zoom level
+    zoomLevel += 0.1;
+    if (zoomLevel > 3) {
+      zoomLevel = 3;
+    }
+  },
+  zoomOut: () => {
+    // Decrease the zoom level
+    zoomLevel -= 0.1;
+    if (zoomLevel < 1) {
+      zoomLevel = 1;
+    }
+  }
+};
 
-          // set range input to control zoom
-          zoomControl.min = capabilities.zoom.min;
-          zoomControl.max = capabilities.zoom.max;
-          zoomControl.step = capabilities.zoom.step;
-          zoomControl.value = capabilities.zoom.current;
+// Call the enabled feature functions on an interval
+setInterval(() => {
+  // Draw current video frame on the canvas with zoom
+  canvasContext.drawImage(
+    videoElement,
+    -(canvasElement.width * zoomLevel - canvasElement.width) / 2,
+    -(canvasElement.height * zoomLevel - canvasElement.height) / 2,
+    canvasElement.width * zoomLevel,
+    canvasElement.height * zoomLevel,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
 
-          // add event listener to zoom control
-          zoomControl.addEventListener('input', function(event) {
-            // set zoom level
-            track.applyConstraints({ advanced: [ { zoom: event.target.value } ] });
-          });
-        }
-      }
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}
+  // Call the enabled feature functions
+  for (const featureName of featureNames) {
+    const feature = features[featureName];
+    if (feature) {
+      feature();
+    }
+  }
+}, 100); // Repeat every 100 milliseconds (10 frames per second)
