86204ec9485eb9a40f8587081c57fbd4388b0074
Anatoly <waveinspectro@gmail.com>
2024-03-21 15:39:59 +0100

    Delete other/ec.js

--- other/ec.js
+++
@@ -1,8 +0,0 @@
-console.log("KEKS")
-function calculateTotalButtons() {
-  const buttons = document.querySelectorAll('button'); // Select all button elements
-  return buttons.length; // Return the total number of buttons
-}
-
-// Output the total number of buttons to the console
-console.log('Total number of buttons:', calculateTotalButtons());
