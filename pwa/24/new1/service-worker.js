3de927ed44b4cedef904cd219cade392e0d93c9b
Anatoly <waveinspectro@gmail.com>
2024-06-11 15:37:03 +0200

    Update service-worker.js

--- pwa/24/new1/service-worker.js
+++ pwa/24/new1/service-worker.js
@@ -2,11 +2,10 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        '',
-        'index.html',
-        'manifest.json',
-        'styles.css', // if you have a CSS file
-        'app.js' // if you have a JS file
+        '/test/pwa/24/new1/index.html',
+        '/test/pwa/24/new1/manifest.json',
+        '/test/pwa/24/new1/styles.css', // if you have a CSS file
+        '/test/pwa/24/new1/app.js' // if you have a JS file
       ]);
     })
   );
